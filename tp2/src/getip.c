3d3287abac2be42bbb0f466e0110994cca9c4238
Joao-Ramos <joaoramos-13@hotmail.com>
2015-12-13 23:55:11 +0000

    made progress in myClientTCP. Already receives first ip address

--- tp2/src/getip.c
+++ tp2/src/getip.c
@@ -3,11 +3,15 @@
 #include <errno.h> 
 #include <netdb.h> 
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h> 
+#include <arpa/inet.h>
 
 int main(int argc, char *argv[])
 {
-	struct hostent *h;
+    struct hostent *h;
+    struct in_addr **addr_list;
+    int i;
 
     if (argc != 2) {  
         fprintf(stderr,"usage: getip address\n");
@@ -16,15 +20,15 @@ int main(int argc, char *argv[])
 
     /*
     struct hostent {
-    	char    *h_name;       //Official name of the host. 
-    	char    **h_aliases;   //A NULL-terminated array of alternate names for the host. 
-    	int     h_addrtype;    //The type of address being returned; usually AF_INET.
-    	int     h_length;      //The length of the address in bytes.
-    	char    **h_addr_list; //A zero-terminated array of network addresses for the host. 
-				//Host addresses are in Network Byte Order. 
+        char    *h_name;       //Official name of the host. 
+        char    **h_aliases;   //A NULL-terminated array of alternate names for the host. 
+        int     h_addrtype;    //The type of address being returned; usually AF_INET.
+        int     h_length;      //The length of the address in bytes.
+        char    **h_addr_list; //A zero-terminated array of network addresses for the host. 
+                //Host addresses are in Network Byte Order. 
     };
 
-    #define h_addr h_addr_list[0]	The first address in h_addr_list. 
+    #define h_addr h_addr_list[0]   The first address in h_addr_list. 
 
 */
     if ((h=gethostbyname(argv[1])) == NULL) {  
@@ -33,7 +37,11 @@ int main(int argc, char *argv[])
     }
 
     printf("Host name  : %s\n", h->h_name);
-    printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
-
+    addr_list = (struct in_addr **)h->h_addr_list;
+    printf("  IP addresses: ");
+    for(i = 0; addr_list[i] != NULL; i++) {
+        printf("%s ", inet_ntoa(*addr_list[i]));
+    }
+    printf("\n");
     return 0;
 }
--- tp2/src/myClientTCP.c
+++ tp2/src/myClientTCP.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <sys/sockfdet.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
@@ -10,9 +10,10 @@
 #include <signal.h>
 #include <netdb.h>
 #include <strings.h>
+#include <errno.h> 
 
-#define SERVER_PORT 6000
-#define SERVER_ADDR "192.168.28.96"
+#define SERVER_PORT "21"
+#define SIZE 2048
 
 struct ftpStruct{
 	char[64]user;
@@ -21,7 +22,52 @@ struct ftpStruct{
 	char[256] url-path;
 };
 
+struct ipStruct{
+
+	int ip1;
+	int ip2;
+	int ip3;
+	int ip4;
+	int port1;
+	int port2;
+};
+
 struct ftpStruct ftps;
+struct ipStruct ips;
+
+//function to receive answers from the socket
+int receive_answer(int sockfd, char * buf){
+
+	int code;
+	char* rest;
+	memset(buf, 0, SIZE);
+	sleep(2);
+	strcpy(buf, "");
+	recv(sockfd,buf,SIZE, 0);
+	printf("\n\nReceived message:\n\n%s\n\n",buf);
+
+	if(sscanf(buf, "%d %s", code, rest) == 1){
+
+		if(code >= 500)
+		{
+			printf("Failed with code %d!\n", code);
+			exit(1);
+		}
+		else{
+			return;
+		}
+	}
+	return 0;
+}
+
+//function to send an answer through the socket
+int send_answer(int sockfd, char* msg){
+
+	printf("\n\nSending message:\n\n%s\n\n",msg);
+	send(sockfd,msg, strlen(msg),0);
+
+	return 0;
+}
 
 int argvHandler(char* argv){
 
@@ -32,7 +78,20 @@ int argvHandler(char* argv){
     	printf("Copied to password: %s\n",ftps.password);
     	printf("Copied to host: %s\n",ftps.host);
     	printf("Copied to url-path: %s\n",ftps.url-path);
-    	printf("Argv successfully decripted!\n");
+    	printf("Argv successfully decripted!\n\n");
+    	
+    	return 0;
+
+    }
+    if (sscanf(argv, "ftp://%[^/]/%s\n", ftps.host, ftps.url-path) == 2){
+    	
+    	strcpy(ftps.user, "anonymous");
+    	strcpy(ftps.pass, "123");
+    	printf("Copied to user: %s\n",ftps.user);
+    	printf("Copied to password: %s\n",ftps.password);
+    	printf("Copied to host: %s\n",ftps.host);
+    	printf("Copied to url-path: %s\n",ftps.url-path);
+    	printf("Argv successfully decripted!\n\n");
     	
     	return 0;
 
@@ -44,65 +103,92 @@ int argvHandler(char* argv){
 
 int main(int argc, char** argv){
 
-	char * server_addr;
-	struct hostent *h;
 
-        if (argc != 2) {  
-            fprintf(stderr,"usage: client address\n");
-            exit(1);
-        }
+	char * buf[SIZE];
+	char msg[SIZE];
 
-	if ((h=gethostbyname(argv[1])) == NULL) {  
-            herror("gethostbyname");
-            exit(1);
-        }
+	int sockfd;  
+	struct addrinfo host_info, *server_info, *aux, *new_server;
+	struct sockfdaddr_in *addr;
+	int getaddr;
 
-	server_addr = inet_ntoa(*((struct in_addr *)h->h_addr));
-	printf ("Server address is: %s!\n", server_addr);
+    if (argc != 2) {  
+        fprintf(stderr,"usage: ftp://[<user>:<password>@]<host>/<url-path>\n");
+        exit(1);
+    }
 
-	int	sockfd;
-	struct	sockaddr_in server_addr;
-	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
-	int	bytes;
-	
-	/*server address handling*/
-	bzero((char*)&server_addr,sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
-	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
+    if(argvHandler(argv[1] != 0)){
+    	fprintf(stderr,"Argv format is wrong!\n Usage: ftp://[<user>:<password>@]<host>/<url-path>\n");
+    	exit(1);
+    }
+
+	memset(&host_info, 0, sizeof(host_info));
+	host_info.ai_family = AF_INET;
+	host_info.ai_sockfdtype = sockfd_STREAM;
+	host_info.ai_protocol = 0;
+
+	if ((getaddr = getaddrinfo(ftps.host, SERVER_PORT, &host_info, &server_info)) != 0) {
+	    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(getaddr));
+	    exit(1);
+	}
     
-	/*open an TCP socket*/
-	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-    		perror("socket()");
-        	exit(0);
-    	}
-	
-	/*connect to the server*/
-    	if(connect(sockfd, 
-	           (struct sockaddr *)&server_addr, 
-		   sizeof(server_addr)) < 0){
-        	perror("connect()");
-		exit(0);
+	for(aux = server_info; aux != NULL; aux = aux->ai_next) {
+	    if ((sockfd = sockfdet(aux->ai_family, aux->ai_sockfdtype, aux->ai_protocol)) == -1) {
+			perror("Failed to create sockfdet!");
+			continue;
+	    }
+
+	    if (connect(sockfd, aux->ai_addr, aux->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("Failed to connect to host!");
+			continue;
+	    }
+
+	    break;
 	}
 
+	if (aux == NULL) {
+	    fprintf(stderr, "Failed to connect!\n");
+	    exit(2);
+	}
+
+	printf("Host name is:	%s\n", ftps.host);
+	addr = (struct sockfdaddr_in *)server_info->ai_addr; 
+	printf("Server address is:	 %s\n", inet_ntoa((struct in_addr)addr->sin_addr));
+
 	//recever resposta do servidor
-    
+    receive_answer(sockfd, buf);
     //send user
-	bytes = write(sockfd, buf, strlen(buf));
-	printf("Bytes escritos %d\n", bytes);
+    memset(msg, 0, SIZE);)
+    strcpy(msg, "USER ");
+    strcat(msg, ftps.user);
+    send_answer(sockfd, msg);
+
 	//receive 
-
+	receive_answer(sockfd, buf);
 	//send pass
+	memset(msg, 0, SIZE);)
+    strcpy(msg, "PASS ");
+    strcat(msg, ftps.pass);
+    send_answer(sockfd, msg);
 
+    //receive 
+	receive_answer(sockfd, buf);
 	//send pasv
+	memset(msg, 0, SIZE);)
+    strcpy(msg, "PASV");
+    send_answer(sockfd, msg);
+    receive_answer(sockfd, buf);
 
 	//receive answer as 193.231.321.31.2312.3 // check page from guião 1ªa aula
 	// res is the buffer where the answer is saved
-	if(6 != sscanf(res, "%*[^(](%d,%d,%d,%d,%d,%d)\n", &(ctrl_ftp.pasv[0]), &(ctrl_ftp.pasv[1]), &(ctrl_ftp.pasv[2]), &(ctrl_ftp.pasv[3]), &(ctrl_ftp.pasv[4]), &(ctrl_ftp.pasv[5])))
+	if(sscanf(buf, "%*[^(](%d,%d,%d,%d,%d,%d)\n", ips.ip1, ips.ip2, ips.ip3, ips.ip4, ips.port1, ips.port2) != 6)
 		{
 			printf("Could not read the 6 bytes from the server response: %s\n", res);
 			return -1;
 		}
+
 	close(sockfd);
+	freeaddrinfo(server_info);
 	exit(0);
 }
